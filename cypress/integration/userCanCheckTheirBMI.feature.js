0ce45ca441bd6f9f598465e5d458c3cc6ab7abc6
Emma_Thalen <thlalen.emma@gmail.com>
2020-01-23 14:50:06 +0100

    second test in userCanCheckTheirBMI passed

--- cypress/integration/userCanCheckTheirBMI.feature.js
+++ cypress/integration/userCanCheckTheirBMI.feature.js
@@ -3,3 +3,10 @@ describe('User can check their Bmi', () => {
 		cy.visit('http://localhost:3001')
 	})
 })
+
+describe('User can check their Bmi', () => {
+	it('user can enter weight and height', () => {
+		cy.visit('http://localhost:3001')
+		cy.contains('BMI Calculator')
+	})
+})
